427e121efd0d0d9ab6755cd5857d32fe71963dc5
vasilmoskov <moskov.vasil@gmail.com>
2022-12-11 20:19:01 +0200

    ensure we have at least empty array of ingredients by transforming response data in a pipe

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {RecipeService} from "../recipes/recipe.service";
 import {Recipe} from "../recipes/recipe.model";
+import {map} from "rxjs";
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
@@ -23,6 +24,16 @@ export class DataStorageService {
   fetchRecipes() {
     this.http
       .get<Recipe[]>('https://ng-course-recipe-book-cabb2-default-rtdb.europe-west1.firebasedatabase.app/recipes.json')
+      .pipe(
+        map(recipes => {
+          return recipes.map(recipe => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : []
+            }
+          })
+        })
+      )
       .subscribe(recipes => {
         this.recipeService.setRecipes(recipes);
       });
