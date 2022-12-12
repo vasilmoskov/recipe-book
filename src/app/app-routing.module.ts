4c829c6675ae50d64002e42be2117078bdc0d5d2
vasilmoskov <moskov.vasil@gmail.com>
2022-12-12 16:01:49 +0200

    create auth component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import {RecipeDetailsComponent} from "./recipes/recipe-details/recipe-details.co
 import {RecipeStartComponent} from "./recipes/recipe-start/recipe-start.component";
 import {RecipeEditComponent} from "./recipes/recipe-edit/recipe-edit.component";
 import {RecipeResolverService} from "./recipes/recipe-resolver.service";
+import {AuthComponent} from "./auth/auth.component";
 
 const appRoutes: Routes = [
   {
@@ -40,6 +41,10 @@ const appRoutes: Routes = [
   {
     path: 'shopping-list',
     component: ShoppingListComponent
+  },
+  {
+    path: 'auth',
+    component: AuthComponent
   }
 ];
 
