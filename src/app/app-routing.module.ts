80d90c1f0093d797e8e28b08fda01ac2d0f1fdd7
Moskov, Vasko <vasko.moskov@experian.com>
2022-12-16 15:02:46 +0200

    implement lazy loading for recipes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
-import {AuthComponent} from "./auth/auth.component";
 
 const appRoutes: Routes = [
   {
     path: '',
     redirectTo: 'recipes',
     pathMatch: 'full'
+  },
+  {
+    path: 'recipes',
+    loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesModule)
   }
 ];
 
--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -5,8 +5,6 @@ import {RecipeDetailsComponent} from "./recipe-details/recipe-details.component"
 import {RecipesComponent} from "./recipes.component";
 import {RecipeStartComponent} from "./recipe-start/recipe-start.component";
 import {RecipeEditComponent} from "./recipe-edit/recipe-edit.component";
-import {CommonModule} from "@angular/common";
-import {AppRoutingModule} from "../app-routing.module";
 import {ReactiveFormsModule} from "@angular/forms";
 import {RecipesRoutingModule} from "./recipes-routing.module";
 import {SharedModule} from "../shared/shared.module";
@@ -21,7 +19,6 @@ import {SharedModule} from "../shared/shared.module";
     RecipeEditComponent,
   ],
   imports: [
-    AppRoutingModule,
     ReactiveFormsModule,
     RecipesRoutingModule,
     SharedModule
