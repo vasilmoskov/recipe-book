c8967af537ab78934f40421170b4a158d685d2a8
vasilmoskov <moskov.vasil@gmail.com>
2022-12-17 17:29:35 +0200

    create action for AddIngredients, dispatch it in recipe service and use shoppingListReducer to update the App State

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -3,6 +3,8 @@ import {Injectable} from "@angular/core";
 import {Ingredient} from "../shared/ingredient.model";
 import {ShoppingListService} from "../shopping-list/shopping-list.service";
 import {Subject} from "rxjs";
+import {Store} from "@ngrx/store";
+import * as ShoppingListActions from "../shopping-list/store/shopping-list.actions";
 
 @Injectable()
 export class RecipeService {
@@ -22,14 +24,16 @@ export class RecipeService {
 
   recipeManipulated = new Subject<Recipe[]>();
 
-  constructor(private shoppingListService: ShoppingListService) {}
+  constructor(private shoppingListService: ShoppingListService,
+              private store: Store<{shoppingList: {ingredients: Ingredient[]}}>) {}
 
   getRecipes() {
     return this.recipes.slice();
   }
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
-    this.shoppingListService.addIngredients(ingredients);
+    this.store.dispatch(new ShoppingListActions.AddIngredients(ingredients));
+    // this.shoppingListService.addIngredients(ingredients);
   }
 
   getRecipe(index: number) {
